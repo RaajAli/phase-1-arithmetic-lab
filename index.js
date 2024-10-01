8131036d8e4df8a998a52c2adc4bae0c13009222
Raaj Ali <raajrali1999@gmail.com>
2024-10-01 13:29:33 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = 1;
+
+const num3 = 48;
+const num4 = 12;
+const mod = num3 / num4;
+
+const max = 20;
